813b25a9a0969cf11bbbb01ecff1d0f163be4a8e
Vivexplorer <137091298+Vivexplorer@users.noreply.github.com>
2025-10-12 17:00:21 -0400

    V1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
             .mass(11.88412);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(1.0)
+            .maxPower(1)
             .rightFrontMotorName("rightFrontDrive")
             .rightRearMotorName("rightBackDrive")
             .leftRearMotorName("leftBackDrive")
