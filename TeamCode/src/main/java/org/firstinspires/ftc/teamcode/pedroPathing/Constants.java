8de84b88cc1e34435701d638dd4a1ea23d1d2e97
Vivexplorer <137091298+Vivexplorer@users.noreply.github.com>
2025-10-20 17:27:55 -0400

    TuningUpdates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -12,7 +14,14 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(11.88412);
+            .mass(11.88412)
+            .forwardZeroPowerAcceleration(-25.265)
+            .lateralZeroPowerAcceleration(-78.167)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.2, 0, 0.005, 0.03))
+            .headingPIDFCoefficients(new PIDFCoefficients(1, 0, 0.1, 0.025))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.02, 0, 0.0002, 0.3, 0.01))
+            .centripetalScaling(0.0004);
+
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -23,29 +32,33 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(77.986)
+            .yVelocity(62.715);
 
     public static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
-            .forwardTicksToInches(.001989436789)
-            .strafeTicksToInches(.001989436789)
-            .turnTicksToInches(.001989436789)
+            .turnTicksToInches(0.003229438)
             .leftPodY(6.7)
             .rightPodY(-6.7)
             .strafePodX(-5)
             .leftEncoder_HardwareMapName("leftFrontDrive")
             .rightEncoder_HardwareMapName("rightFrontDrive")
             .strafeEncoder_HardwareMapName("rightBackDrive")
-            .leftEncoderDirection(Encoder.REVERSE)
+            .leftEncoderDirection(Encoder.FORWARD)
             .rightEncoderDirection(Encoder.REVERSE)
-            .strafeEncoderDirection(Encoder.REVERSE);
+            .strafeEncoderDirection(Encoder.FORWARD)
+            .strafeTicksToInches(00.002943746)
+            .forwardTicksToInches(0.0029757745);
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1.35, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .threeWheelLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
+
+
                 .build();
     }
 }
