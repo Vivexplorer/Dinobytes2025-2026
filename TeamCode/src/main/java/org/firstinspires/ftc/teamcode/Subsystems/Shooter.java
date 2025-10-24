ac303b8268e331c14a3f01382e1901781989eaaa
Vivexplorer <137091298+Vivexplorer@users.noreply.github.com>
2025-10-24 17:25:24 -0400

    Shooter Subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-import java.util.Timer;
+
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -9,13 +11,36 @@ public class Shooter {
 
     static HardwareMap hardwareMap;
 
-    Timer shootingBallTimer;
+
+    public static double kP, kI, kD, kF;
+
+    static PIDFController pidf;
+
+    static double velocityWanted = 125.0/60;
+
+
 
     public static void initShooterClass() {
         shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        pidf = new PIDFController(kP, kI, kD, kF);
+        pidf.setPIDF(kP, kI, kD, kF);
+
     }
 
-    public static void ShootBall() {
 
+
+
+
+    public static void ShootBallLoop() {
+        while (!pidf.atSetPoint()) {
+
+            double error = pidf.calculate(
+                    shooter.getVelocity(), velocityWanted
+            );
+
+            shooter.setVelocity(error);
+        }
     }
 }
