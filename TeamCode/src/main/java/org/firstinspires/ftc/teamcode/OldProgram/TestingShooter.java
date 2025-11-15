2f311f76ec7f582c89b7b5f7c99792e8c694e4f9
Vivexplorer <Vivaanshah06@gmail.com>
2025-11-15 07:18:03 -0500

    Disabled THINGS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OldProgram/TestingShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OldProgram/TestingShooter.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.OldProgram;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -7,6 +8,7 @@ import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Subsystems.Outtake;
 
+@Disabled
 @TeleOp
 public class TestingShooter extends OpMode {
 
